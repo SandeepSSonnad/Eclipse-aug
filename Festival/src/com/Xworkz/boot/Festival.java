2cce62493ac81eca92df84ab5a4e3cdf0254e0ec
Sandeep Sonnad <sandeepssonnadxworks@gmail.com>
2022-10-11 09:34:45 +0530

    Copy files

---
+++ Festival/src/com/Xworkz/boot/Festival.java
@@ -0,0 +1,13 @@
+package com.Xworkz.boot;
+
+public class Festival {
+
+      public Festival()
+      {
+    	  System.out.println("This is Explict Constructer");
+      }
+      
+	
+	}
+
+
---
+++ Festival/src/com/Xworkz/boot/FestivalRunner.java
@@ -0,0 +1,21 @@
+package com.Xworkz.boot;
+import  com.Xworkz.boot.Festival.*;
+
+public class FestivalRunner {
+
+	public static void main(String[] args) {
+		
+		new Festival();
+		new Festival();
+		new Festival();
+		new Festival();
+		new Festival();
+		System.out.println("The Java");
+		
+		
+		
+		
+		
+	}
+
+}
